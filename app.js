ab15d89d97b0d66ddb3e5ea31cf91d3539810582
Ty Aponte <aaustinsk8@aol.com>
2022-12-05 15:55:34 -0800

    built out objects, some utilities

--- app.js
+++ app.js
@@ -11,3 +11,65 @@
 //TODO Store the results for each location in a separate array… perhaps as a property of the object representing that location.
 //TODO Display the values of each array as unordered lists in the browser.
 //TODO Calculating the sum of these hourly totals
+//***********************GLOBALS**************************************************
+let hours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
+// *********************UTILITIES*************************************************
+
+function rngCookies(minCust,maxCust){
+  //found on MDN docs
+  return Math.floor(Math.random() * (maxCust - minCust +1) + minCust);
+}
+
+
+// objectTemplate
+// let ={
+//   name: '',
+//   minCust:  ,
+//   maxCust: ,
+//   avgCookiesBought:
+//   cookiesBought: [],
+// };
+//******************************** OBJECT LITERALS********************************
+
+let tokyo ={
+  name: 'Tokyo',
+  minCust: 3,
+  maxCust: 24,
+  avgCookiesBought: 1.2,
+  cookiesBought: [],
+};
+
+let seattle ={
+  name: 'Seattle',
+  minCust:  23,
+  maxCust: 65,
+  avgCookiesBought: 6.3,
+
+  cookiesBought: [],
+};
+
+let dubai ={
+  name: 'Dubai',
+  minCust: 11,
+  maxCust: 38,
+  avgCookiesBought: 3.7,
+  cookiesBought: [],
+};
+
+let paris={
+  name: 'Paris',
+  minCust:  20,
+  maxCust: 38,
+  avgCookiesBought: 2.3,
+  cookiesBought: [],
+};
+
+let lima={
+  name: 'Lima',
+  minCust:  2,
+  maxCust: 16,
+  avgCookiesBought: 4.6,
+  cookiesBought: [],
+};
+
