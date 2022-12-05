7aeb850447d23af140fd5336b153169fd1f16fa0
Ty Aponte <aaustinsk8@aol.com>
2022-12-05 15:19:00 -0800

    initialized with todo list and .gitignore .lintr

---
+++ app.js
@@ -0,0 +1,13 @@
+// needs a proof of concept application to calculate the number of cookies each location must make every day so that it can manage its supplies inventory and baking schedule. The number of cookies to make depends on the hours of operation (6:00 AM to 8:00 PM for all locations) and a few factors unique to each location:
+
+// TODO The minimum number of customers per hour.
+// TODO The maximum number of customers per hour.
+// TODO The average number of cookies purchased per customer.
+// TODO Because we are early in the life of this business, we will need to build an application that is adaptable.
+// TODO create separate JS object literals for each shop location. Each location will be responsible for generating sales data and providing output on an html document.
+//TODO Store the min/max hourly customers, and the average cookies per customer, in object properties.
+//TODO Use a method of that object to generate a random number of customers per hour. Objects/Math/random
+//TODO Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated.
+//TODO Store the results for each location in a separate array… perhaps as a property of the object representing that location.
+//TODO Display the values of each array as unordered lists in the browser.
+//TODO Calculating the sum of these hourly totals
