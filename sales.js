88da563ee5084ac98e650cd6dda213d7a4ea120a
Ty Aponte <aaustinsk8@aol.com>
2022-12-07 22:00:36 -0800

    i can no longer see localhost. tried a fix, didntwork

--- sales.js
+++ sales.js
@@ -16,21 +16,61 @@ let storeArray = [];
 //WINDOW INTO DOM TEST
 let storeSection = document.getElementById('stores');
 
-// *********************UTILITIES*************************************************
+function header(){
+  let trElem = document.createElement('tr');
+  storeSection.appendChild(trElem);
 
+  let thElem = document.createElement('th');
+  thElem.textContent = 'stores';
+  trElem.appendChild(thElem);
+
+  for (let j = 0; j < hours.length; j++) {
+    thElem = document.createElement('th');
+    thElem.textContent = hours[j];
+    trElem.appendChild(thElem);
+  }
+  thElem = document.createElement('th');
+  thElem.textContent = 'total';
+  trElem.appendChild(thElem);
+}
+
+header();
+
+Store.prototype.render = function() {
+
+  console.dir('stores', storeSection);
+  let trElem = document.createElement('tr');
+  storeSection.appendChild(trElem);
+
+  let tdElem = document.createElement('td');
+  tdElem.textContent = this.name;
+  trElem.appendChild(tdElem);
+
+  for (let i = 0; i < hours.length; i++) {
+    let tableData = document.createElement('td');
+    tableData.textContent = this.cookiesBought[i];
+    console.log(this.cookiesBought[i]);
+
+    trElem.appendChild(tableData);
+
+  }
+  let totalTableData = document.createElement('td');
+  totalTableData.textContent = this.total;
+  trElem.appendChild(totalTableData);
+};
+
+let grandTotals = document.createElement('tr');
+storeSection.appendChild(grandTotals);
 
 
 //*********************CONSTRUCTOR FUNCTION
 
-function Store(name,minCust,maxCust,avgCookiesBought,customerPerHour,cookieSales){
+function Store(name,minCust,maxCust,avgCookiesBought){
   this.name = name;
   this.minCust = minCust;
   this.maxCust = maxCust;
-  this.avgCookiesBought = avgCookiesBought;
+  this.avgCookiesBought = [];
   this.total = 0;
-  this.customerPerHour = customerPerHour;
-  this.cookieSales = cookieSales;
-  this.hours = hours;
 }
 
 //***********PROTOTYPE METHODS....I HOPE
@@ -42,55 +82,60 @@ function Store(name,minCust,maxCust,avgCookiesBought,customerPerHour,cookieSales
 
 
 
-Store.prototype.render = function(){
-  let articleElem = document.createElement('article');
-  storeSection.appendChild(articleElem);
 
-  let h2Elem = document.createElement('h2');
-  h2Elem.innerText = this.name;
-  articleElem.appendChild(h2Elem);
 
-  let ulElem = document.createElement('ul');
-  ulElem.innerText = this.minCust;
-  articleElem.appendChild(ulElem);
+let Tokyo = new Store('Tokyo',3,24,1.2,[],0);
+let Seattle = new Store('Seattle',23,65,6.3,[],0);
+let Dubai = new Store('Dubai',11,38,3.7,[],0);
+let Paris = new Store('Paris',20,38.,2.3,[],0);
+let Lima = new Store('Lima',2,16,4.6,[],0);
 
-  let p2Elem = document.createElement('p2');
-  p2Elem.innerText = this.maxCust;
-  articleElem.appendChild(p2Elem);
 
-  let p3Elem = document.createElement('p3');
-  p3Elem.innerText = this.avgCookiesBought;
-  articleElem.appendChild(p3Elem);
+storeArray.push=[Tokyo, Seattle, Dubai, Paris, Lima ];
 
-  let p4Elem = document.createElement('p4');
-  p4Elem.innertext = this.total;
-  articleElem.appendChild(p4Elem);
 
-  let p5Elem = document.createElement('p5');
-  p5Elem.innerText = this.total;
-  articleElem.appendChild(p5Elem);
+// storeArray = [{Tokyo}, {Seattle}];
+
+
+
+// function renderAll(){
+//   for(let i = 0;i<storeArray.length; i++){
+//     // renderHelper[i].randomCust();
+//     storeArray[i].render();
+//   }
+// }
+
+// renderAll();
+
+Store.prototype.getRandomInt = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 
-let tokyo = new Store('Tokyo',3,24,1.2);
-let seattle = new Store('Seattle',23,65,6.3);
-let dubai = new Store('Dubai',11,38,3.7);
-let paris = new Store('Paris',20,38.,2.3);
-let lima = new Store('Lima',2,16,4.6);
-
-
-storeArray.push(tokyo, seattle, dubai, paris, lima );
-
-//storeArray = [{tokyo}, {seattle}];
-
-
-
-function renderAll(){
-  for(let i = 0;i<storeArray.length; i++){
-    // renderHelper[i].randomCust();
-    storeArray[i].render();
+Store.prototype.cookieSales = function () {
+  for (let i = 0; i < hours.length; i++) {
+    let avgCookie = Math.floor(this.getRandomInt(this.minCust, this.maxCust) * this.avgCookiesBought);
+    this.cookiesBought.push(avgCookie);
+    this.total = this.total + this.cookiesBought[i];
   }
-}
+};
 
-renderAll();
 
+Seattle.cookieSales();
+Tokyo.cookieSales();
+Dubai.cookieSales();
+Paris.cookieSales();
+Lima.cookieSales();
+
+Seattle.render();
+Tokyo.render();
+Dubai.render();
+Paris.render();
+Lima.render();
+
+
+console.log(Seattle);
+console.log(Tokyo);
+console.log(Dubai);
+console.log(Paris);
+console.log(Lima);
